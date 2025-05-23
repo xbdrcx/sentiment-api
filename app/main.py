8864643b937b5d80b31aa4bf2f1ed02f97232a90
Bruno Cruz <xbrunocruzx@outlook.pt>
2025-05-23 15:20:29 +0100

    commit

--- app/main.py
+++ app/main.py
@@ -12,9 +12,7 @@ router.include_router(router, prefix="/events", tags=["events"])
 
 @router.post("/", dependencies=[])
 def handle_event(data: EventSchema) -> Response:
-    
     print(data)
-
     return Response(
         content=json.dumps({"message": "Data received!"}),
         status_code=HTTPStatus.ACCEPTED,
